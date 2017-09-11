d496d127536b1f3483653c1a8ba1f48100a4d88a
Maïté THOMIAS <maite.thomias@gmail.com>
2017-09-11 13:12:23 +0200

    add: table

--- app/02-table/app.js
+++ app/02-table/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const app = angular.module('main', []);
+    const app = angular.module('main', []); // les [] pour initialiser un module, sinon on le recupere
     const ciqual = "../resources/ciqual.csv";
 
     app.component('boofTable', {
@@ -10,7 +10,11 @@
             'ngInject';
             const ctrl = this;
             console.log('this.csv', this.csv);
+
+            // initialisation angular
             ctrl.$onInit = function () {
+
+                // reprise du code d3 pour recuperer le fichier csv
                 d3.text(ctrl.csv, function (err, str) {
                     const dsv = d3.dsvFormat(';');
                     const csvData = dsv.parse(str, function (d) {
@@ -18,28 +22,32 @@
                     });
                     console.log('csvData', csvData);
 
+                    // recuperation des en-tetes
                     ctrl.headers = [];
-
                     for (let p in csvData[0]) {
                         ctrl.headers.push(p);
                     }
-
                     console.log('headers', ctrl.headers);
 
+                    // recuperation des lignes
                     ctrl.rows = csvData.map(function(row) {
                         const result = [];
                         for (let p in row) {
-                            result.push(row[p]);
+                            result.push(row[p]); // push : push dans le dernier index du tableau
                         }
                         return result;
                     });
-                    $scope.$apply();
+                    
+                    // n'etant pas dans une fonction angular lancement de la «digestion» manuel
+                    $scope.$apply(); 
                 });
             }
         },
+
+        // recupere les donnees d'un fichier
         bindings: {
-            csv: '@'
+            csv: '@' // @ : recupere sous forme de chaine de caracteres
         },
-        templateUrl: 'boof-table.html'
+        templateUrl: 'boof-table.html' // lien du fichier a partir duquel on recupere
     });
 })();
