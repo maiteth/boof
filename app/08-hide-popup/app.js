59414da8e25d3a1da87aace134bad4b85a65a446
Maïté THOMIAS <maite.thomias@gmail.com>
2017-09-25 14:02:34 +0200

    add: veil popup

--- app/08-hide-popup/app.js
+++ app/08-hide-popup/app.js
@@ -4,30 +4,51 @@
     const app = angular.module('main', []);
 
     app.component('myCell', {
-        controller: function MyCellCtrl($element) {
+        controller: function MyCellCtrl($element, veil) {
             'ngInject';
             const ctrl = this;
 
             ctrl.isPopupVisible = false;
 
-            ctrl.popup = function () {
+            ctrl.switchPopup = function () {
                 console.log('popup', arguments);
                 const popup = $element.find('boof-popup');
                 console.log('popup', popup);
                 if (ctrl.isPopupVisible) {
                     ctrl.isPopupVisible = false;
-                    popup.css({
-                        display: 'none',
-                    });
+                    veil.off();
                 } else {
                     ctrl.isPopupVisible = true;
-                    popup.css({
-                        display: 'block',
-                    });
+                    veil.on(ctrl);
                 }
             };
         },
         templateUrl: 'my-cell.component.html',
+    });
+
+    app.service('veil', function Veil($document, $rootScope) {
+        'ngInject';
+        const veil = angular.element($document[0].body).find('my-veil');
+        veil.on('click', () => {
+            console.log('click');
+            this.off();
+            this.ctrl.switchPopup();
+            $rootScope.$apply();
+        });
+
+        this.on = function (ctrl) {
+            console.log('on');
+            veil.css({
+                display: 'block'
+            })
+            this.ctrl = ctrl;
+        };
+        this.off = function () {
+            console.log('off');
+            veil.css({
+                display: 'none'
+            })
+        };
     });
 
 
