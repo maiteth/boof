472d32ba78f434c463caa8db82bf2377c625daa8
Maïté THOMIAS <maite.thomias@gmail.com>
2017-09-25 11:17:29 +0200

    add: serve-index

--- server.js
+++ server.js
@@ -9,9 +9,26 @@ const app = express();
 app.use(express.static('.'));
 
 // renvoie un repertoire s'il existe
-// app.use(dir('.'));
-app.use(serveIndex('.', {icons: true}));
+app.use(serveIndex('.', {
+	icons: true,
+	filter: function (filename) {
+		if (filename === 'node_modules') {
+			return false;
+		}
+		return true;
+	},
+	hidden: true,
+	stylesheet: 'server.css',
+	view: 'details',
+}));
+
+app.use(function (req, res, next) {
+	console.log('Not Found', req.url);
+	// res.status(404).sendFile('404.html', {root: '.'});
+	res.status(404).send('<code>Cannot GET ' + req.url + '</code>');
+	// next();
+});
 
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+	console.log('Example app listening on port 8000!');
 });
