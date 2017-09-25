b09a1a97c0a0083ae5e9c5306c25b1ee92864820
Maïté THOMIAS <maite.thomias@gmail.com>
2017-09-25 15:55:48 +0200

    add: popup

--- app/08b-hide-popup/app.js
+++ app/08b-hide-popup/app.js
@@ -5,13 +5,51 @@
 
     app.component('popupable', {
 
-        controller: function Popupable($element, $transclude) {
+        controller: function Popupable($scope, veil) {
             'ngInject';
 
-            console.log('$element', $element.html());
+            const ctrl = this;
+
+            ctrl.isPopupVisible = false;
+
+            $scope.$watch('$ctrl.isPopupVisible', () => {
+                console.log('scope', $scope);
+                if (ctrl.isPopupVisible) {
+                    veil.start(function () {
+                        ctrl.isPopupVisible = false;
+                    });
+                } else {
+                    veil.stop();
+                }
+            });
         },
 
-       templateUrl: 'popupable.html', 
-       transclude: true
+        templateUrl: 'popupable.html',
+        transclude: true
+    });
+
+    app.service('veil', function Veil($document, $rootScope) {
+        'ngInject';
+        const veil = angular.element($document[0].body).find('my-veil');
+        veil.on('click', () => {
+            console.log('click');
+            this.stop();
+            this.onStop();
+            $rootScope.$apply();
+        });
+
+        this.start = function (onStop) {
+            console.log('on');
+            veil.css({
+                display: 'block'
+            })
+            this.onStop = onStop;
+        };
+        this.stop = function () {
+            console.log('stop');
+            veil.css({
+                display: 'none'
+            })
+        };
     });
 })();
