39f99c7bb6fda2a7a93cbaf3084ffdf6a810dd0a
Maïté THOMIAS <maite.thomias@gmail.com>
2017-10-20 10:01:43 +0200

    add: 23 graph

--- app/23-d3-graph/main.js
+++ app/23-d3-graph/main.js
@@ -5,11 +5,11 @@ var margin = {
 	bottom: 20,
 	left: 20,
 	right: 20
-}
+};
 
-var svg = d3.select("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom);
+var svg = d3.select('svg')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom);
 
 var y = d3.scaleLinear()
 	.range([height, 0])
@@ -19,8 +19,8 @@ var yAxis = d3.axisLeft()
 	.scale(y)
 	.tickSize(10);
 
-svg.append("g")
-	.attr("transform", `translate(${(width / 2) + margin.left},${margin.top})`)
+svg.append('g')
+	.attr('transform', `translate(${(width / 2) + margin.left},${margin.top})`)
 	.call(yAxis);
 
 var x = d3.scaleLinear()
@@ -31,6 +31,41 @@ var xAxis = d3.axisBottom()
 	.scale(x)
 	.tickSize(10);
 
-svg.append("g")
-	.attr("transform", `translate(${margin.left}, ${(height / 2) + margin.top})`)
+svg.append('g')
+	.attr('transform', `translate(${margin.left}, ${(height / 2) + margin.top})`)
 	.call(xAxis);
+
+// <path d="M150 0 L75 200 L225 200 Z" />
+
+//The data for our line
+
+
+function plot(callback, color) {
+	var lineData = d3.range(-4, 4.1, 0.1).map(d => {
+		return {
+			x: d,
+			y: callback(d)
+		};
+	});
+
+	//This is the accessor function we talked about above
+	var lineFunction = d3.line()
+		.x(function(d) { return x(d.x); })
+		.y(function(d) { return y(d.y); })
+		.curve(d3.curveCatmullRom.alpha(0.5));
+
+	svg.append('path')
+		.attr('transform', `translate(${margin.left}, ${margin.top})`)
+		.attr('d', lineFunction(lineData))
+		.attr('stroke', color)
+		.attr('stroke-width', 2)
+		.attr('fill', 'none');
+}
+
+plot(function (x) {
+	return x * x;
+}, 'blue');
+
+plot(function (x) {
+	return -x * x;
+}, 'red');
